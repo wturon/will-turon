5b83bbe488cbf5adce5be6673ebd7a313357734a
William Turon <William.Turon@neudesic.com>
2021-05-17 22:17:30 -0400

    basic title card structure

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
+import "./Home.css";
 
 export const Home = (): JSX.Element => {
-    return (
-        <div>Home!</div>
-    );
-}
+  return (
+    <div>
+      <div className="container">
+        <div className="title-group">
+          <h1>Hi, I'm Will.</h1>
+          <p>
+            I'm a Software developer, photographer, and photoshopping
+            enthusiast. I made this website
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
