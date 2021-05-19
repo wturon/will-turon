baff33b7811da6219342c0141ac52e441eabdb6c
William Turon <William.Turon@neudesic.com>
2021-05-19 10:56:25 -0400

    added gradient line and adjusted flex boxes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -11,31 +11,53 @@ export const Home = (): JSX.Element => {
   return (
     <div>
       <div className="container">
-        <div className="title-group">
-          <i className="fab fa-instagram-square"></i>
-          <h1>Hi, I'm Will.</h1>
-          <p>
-            I'm a Software developer, photographer, and photoshopping
-            enthusiast. I made this website
-          </p>
-          <div className="wrapper-col">
-            <a href="https://www.instagram.com/wturon/">
-              <FontAwesomeIcon
-                className="icon"
-                size="2x"
-                icon={faInstagramSquare}
-              />
-            </a>
-            <a href="https://www.linkedin.com/in/william-turon-763a10154/">
-              <FontAwesomeIcon className="icon" size="2x" icon={faLinkedin} />
-            </a>
-            <a href="https://www.youtube.com/channel/UCT3N8AZOsodMSsXnzojTpPA">
-              <FontAwesomeIcon
-                className="icon"
-                size="2x"
-                icon={faYoutubeSquare}
-              />
-            </a>
+        <div className="flex-col full-height">
+          <div className="flex-row">
+            <div className="vertical-line" />
+            <div className="title-group">
+              <h1>
+                Hi, I'm <span className="gradient-text">Will.</span>
+              </h1>
+              <p>
+                I'm a Software developer, photographer, and photoshopping
+                enthusiast. I made this website.
+              </p>
+              <div className="wrapper-col">
+                <a
+                  href="https://www.instagram.com/wturon/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    className="icon"
+                    size="2x"
+                    icon={faInstagramSquare}
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/william-turon-763a10154/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    className="icon"
+                    size="2x"
+                    icon={faLinkedin}
+                  />
+                </a>
+                <a
+                  href="https://www.youtube.com/channel/UCT3N8AZOsodMSsXnzojTpPA"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon
+                    className="icon"
+                    size="2x"
+                    icon={faYoutubeSquare}
+                  />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
