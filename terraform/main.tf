b1bacbafa6c464bb246713d53a0c267e8afc7ad1
Will Turon <wturon1@gmail.com>
2021-08-24 15:54:28 -0400

    update terraform

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "willturontfstate"
+    container_name       = "tfstatedevops"
+    key                  = "tfstatedevops.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "tf-wt-rg" {
+  name     = "tf-wt-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "wt-appservice" {
+  name                = "wt-appserviceplan"
+  location            = azurerm_resource_group.tf-wt-rg.location
+  resource_group_name = azurerm_resource_group.tf-wt-rg.name
+
+  sku {
+    tier = "Standard"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.tf-wt-rg.location
+  resource_group_name = azurerm_resource_group.tf-wt-rg.name
+  app_service_plan_id = azurerm_app_service_plan.wt-appservice.id
+
+  site_config {
+    linux_fx_version = "NODE|12-lts"
+    app_command_line = "pm2 serve /home/site/wwwroot --no-daemon --spa"
+  }
+
+  app_settings = {
+    "WEBSITE_WEBDEPLOY_USE_SCM" = "TRUE"
+  }
+}
