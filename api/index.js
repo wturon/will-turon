632453a56ff1841e83aaf9386b27de9b01a55e02
Will Turon <wturon1@gmail.com>
2021-08-25 14:30:42 -0400

    api init

--- api/index.js
+++ api/index.js
@@ -1,5 +1,28 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const PORT = 8080;
+
+// app.use(express.json());
+app.use(cors());
+
+app.get("/boards", (req, res) => {
+  res.status(200).send({
+    mileage: 100000,
+    name: "Boosted Board",
+  });
+});
+
+// app.post("/tshirt/:id", (req, res) => {
+//   const { id } = req.params;
+//   const { logo } = req.body;
+
+//   if (!logo) {
+//     res.status(418).send({ message: "Oh no!" });
+//   }
+
+//   res.status(200).send({ message: "success!" });
+// });
 
 app.listen(PORT, () =>
   console.log(`We're live on port http://localhost:${PORT}`)
