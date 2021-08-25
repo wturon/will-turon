bba68884cf6d13ed37af096dd379e52069565441
Will Turon <wturon1@gmail.com>
2021-08-25 19:04:09 -0400

    update ci api pipeline

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-// app.use(express.json());
 app.use(cors());
 
 app.get("/boards", (req, res) => {
@@ -12,17 +11,6 @@ app.get("/boards", (req, res) => {
     name: "Boosted Board",
   });
 });
-
-// app.post("/tshirt/:id", (req, res) => {
-//   const { id } = req.params;
-//   const { logo } = req.body;
-
-//   if (!logo) {
-//     res.status(418).send({ message: "Oh no!" });
-//   }
-
-//   res.status(200).send({ message: "success!" });
-// });
 
 app.listen(PORT, () =>
   console.log(`We're live on port http://localhost:${PORT}`)
