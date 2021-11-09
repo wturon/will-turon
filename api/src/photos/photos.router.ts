5386b3c00da3a6287eb00ff76434722acee986da
Will Turon <wturon1@gmail.com>
2021-11-09 16:13:44 -0500

    adding put and get by ID

--- api/src/photos/photos.router.ts
+++ api/src/photos/photos.router.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import * as PhotoService from "./photos.service";
+import { BasePhoto } from "./photo.interface";
 
 export const photosRouter = Router();
 
@@ -10,6 +11,46 @@ photosRouter.get("/", async (req: Request, res: Response) => {
       return res.status(200).send(photos);
     }
     res.status(404).send("Nothing to return");
+  } catch (e: any) {
+    res.status(500).send(e.message);
+  }
+});
+
+photosRouter.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(req.params.id);
+    const photo = await PhotoService.find(id);
+    if (photo) {
+      return res.status(200).send(photo);
+    }
+    res.status(404).send("Item not found");
+  } catch (e: any) {
+    res.send(500).send(e.message);
+  }
+});
+
+photosRouter.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(req.params.id);
+    const existingPhoto = await PhotoService.find(id);
+    const photoUpdate = req.body;
+
+    if (existingPhoto) {
+      const updatedPhoto = PhotoService.update(id, photoUpdate);
+      return res.status(200).json(updatedPhoto);
+    }
+    res.status(404).send("No photo with matching ID");
+  } catch (e: any) {
+    res.status(500).send(e.message);
+  }
+});
+
+photosRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    const photo: BasePhoto = req.body;
+    const newPhoto = await PhotoService.create(photo);
+
+    res.status(201).json(newPhoto);
   } catch (e: any) {
     res.status(500).send(e.message);
   }
