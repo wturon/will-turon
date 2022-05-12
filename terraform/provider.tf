4943d748df9de0b895f1ac5ff1fd78ae07a9b2ad
Will Turon <wturon1@gmail.com>
2022-05-12 12:15:35 -0400

    updating provider file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
-
-  required_version = ">=0.14.9"
   backend "s3" {
     bucket = "wturon-personal-remote-state"
     key    = "states/terraform.tfstate"
