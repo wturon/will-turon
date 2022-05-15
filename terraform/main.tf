150434407f3aaae421a7e392cff05927f17f5b86
Will Turon <wturon1@gmail.com>
2022-05-15 07:53:13 -0400

    adding domain association

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,6 @@ resource "aws_amplify_app" "frontend" {
   }
 }
 
-
 resource "aws_amplify_branch" "main" {
   app_id      = aws_amplify_app.frontend.id
   branch_name = "main"
@@ -53,4 +52,21 @@ resource "aws_amplify_branch" "main" {
 resource "aws_route53_zone" "zone_apex" {
   name    = "willturon.com"
   comment = "Hosted Zone for willturon.com"
+}
+
+resource "aws_amplify_domain_association" "fe_domain" {
+  app_id      = aws_amplify_app.frontend.id
+  domain_name = "willturon.com"
+
+  # https://example.com
+  sub_domain {
+    branch_name = aws_amplify_branch.main.branch_name
+    prefix      = ""
+  }
+
+  # https://www.example.com
+  sub_domain {
+    branch_name = aws_amplify_branch.main.branch_name
+    prefix      = "www"
+  }
 }
