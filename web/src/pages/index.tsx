6b6c951552d62994028b8b698196d4bfce6119e1
Will Turon <wturon1@gmail.com>
2023-04-08 20:56:25 -0400

    adding picture

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="flex h-full flex-col items-center justify-center">
+      <Image src="/me.jpg" alt="Will" width={100} height={100} />
       <h1 className="text-3xl font-bold text-white">Hi, I'm Will! 👋🏻</h1>
     </div>
   )
